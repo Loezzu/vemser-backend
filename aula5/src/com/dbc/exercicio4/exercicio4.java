6a78b2fa6125aa4e41e91953ec5ce34ee2085c8f
Luiz Souza <luixsouza12@outlook.com>
2022-02-23 19:12:49 -0300

    Homework4

--- aula5/src/com/dbc/exercicio4/exercicio4.java
+++ aula5/src/com/dbc/exercicio4/exercicio4.java
@@ -8,26 +8,36 @@ public class exercicio4 {
         Scanner scan = new Scanner(System.in);
         Stack<Integer> pilha = new Stack<>();
 
-        System.out.println("Informe um valor: ");
-        int valor = scan.nextInt();
-        if (valor % 2 == 0) {
-            pilha.add(valor);
-        }
+        int x = 1;
+        while(x <= 15){
 
-        do {
-            System.out.println("Informe outro valor: ");
-            valor = scan.nextInt();
-            if (valor % 2 == 0) {
-                pilha.add(valor);
+            System.out.println("Digite o numero " + x);
+            int numero = scan.nextInt();
+            if(numero % 2 == 0){
+                System.out.println("Par! Adicionando na lista");
+                pilha.add(numero);
+            }else if(!pilha.isEmpty()){
+                System.out.println("Impar! Retirando um valor da lista");
+                pilha.pop();
+            }else{
+                System.out.println("Pilha vazia! Não é possível retirar um número.");
             }
-        } while (valor < 15);
-
-
-        while (!pilha.isEmpty()) {
-            System.out.println(pilha.pop());
+            x++;
 
         }
 
+        System.out.println("================================");
+        System.out.println("Esvaziando pilha!");
+        while(!pilha.isEmpty()){
+            System.out.println("Retirando: ");
+            System.out.println(pilha.pop());
+        }
+
+        System.out.println("================================");
+        System.out.println("Pilha vazia: ");
+        System.out.println(pilha);
+
+        scan.close();
 
     }
 }
