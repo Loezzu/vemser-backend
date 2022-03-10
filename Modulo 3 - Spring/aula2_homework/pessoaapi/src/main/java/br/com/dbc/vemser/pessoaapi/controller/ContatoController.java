9fea319ef9ea0aaceaf55c2a49300abbc682a88f
Luiz Souza <luixsouza12@outlook.com>
2022-03-10 19:13:05 -0300

    aula2_homework criado

---
+++ Modulo 3 - Spring/aula2_homework/pessoaapi/src/main/java/br/com/dbc/vemser/pessoaapi/controller/ContatoController.java
@@ -0,0 +1,47 @@
+package br.com.dbc.vemser.pessoaapi.controller;
+
+
+import br.com.dbc.vemser.pessoaapi.entity.Contato;
+import br.com.dbc.vemser.pessoaapi.entity.Pessoa;
+import br.com.dbc.vemser.pessoaapi.service.ContatoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contato")
+public class ContatoController {
+
+    private ContatoService contatoService;
+
+    public ContatoController() {
+        contatoService = new ContatoService();
+    }
+
+    @GetMapping  // localhost:8080/contato
+    public List<Contato> list() {
+        return contatoService.list();
+    }
+
+    @GetMapping("/{idPessoa}")// localhost:8080/contato/4
+    public List<Contato> listByIdPessoa(@PathVariable("idPessoa") Integer idPessoa) {
+        return contatoService.listByIdPessoa(idPessoa);
+    }
+
+    @PostMapping
+    public Contato create(@RequestBody Contato contato) {
+        return contatoService.create(contato);
+    }
+
+    @PutMapping("/{idContato}")
+    public Contato update(@PathVariable("idContato") Integer idContato, @RequestBody Contato contatoAtualizar) throws Exception  {
+        return contatoService.update(idContato, contatoAtualizar);
+    }
+
+    @DeleteMapping("/{idContato}")
+    public void delete(@PathVariable("idContato") Integer idContato) throws Exception  {
+        contatoService.delete(idContato);
+    }
+
+
+}
