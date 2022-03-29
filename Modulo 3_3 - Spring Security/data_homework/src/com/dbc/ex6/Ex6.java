b3650560b551d0a56f5b59293820990e42a68515
Luiz Souza <luixsouza12@outlook.com>
2022-03-29 16:20:29 -0300

    correcao homework

--- Modulo 3_3 - Spring Security/data_homework/src/com/dbc/ex6/Ex6.java
+++ Modulo 3_3 - Spring Security/data_homework/src/com/dbc/ex6/Ex6.java
@@ -6,30 +6,32 @@ import java.time.temporal.ChronoUnit;
 public class Ex6 {
     public static void main(String[] args) {
 
-        LocalDateTime show = LocalDateTime.of(2024, 9, 14, 18,30);
-        LocalDateTime hoje = LocalDateTime.now();
+        LocalDateTime show = LocalDateTime.of(2024, 9, 14, 18,30).atZone(ZoneId.of("UTC+01:00")).withZoneSameInstant(ZoneId.of("UTC-03:00")).toLocalDateTime();
+        LocalDateTime hoje = LocalDateTime.now().atZone(ZoneId.of("UTC-03:00")).toLocalDateTime();
 
-        //1 jeito
-        Period periodo = Period.between(show.toLocalDate(), hoje.toLocalDate());
-        Duration duracao = Duration.between(show, hoje);
-        System.out.println(periodo);
-        System.out.println(duracao);
+        System.out.println("Hora atual : " + hoje);
+        System.out.println("Hora do show: " + show);
 
-        //2 jeito
-        long anos = ChronoUnit.YEARS.between(hoje, show);
-        long meses  = ChronoUnit.MONTHS.between(hoje, show);
-        long dias = ChronoUnit.DAYS.between(hoje, show);
-        long minutes = ChronoUnit.MINUTES.between(hoje, show);
-        long hours = ChronoUnit.HOURS.between(hoje, show);
-        long seconds = ChronoUnit.SECONDS.between(hoje, show);
-        System.out.println(anos);
-        System.out.println(meses);
-        System.out.println(dias);
-        System.out.println(hours);
-        System.out.println(minutes);
-        System.out.println(seconds);
+        LocalDateTime tempoDescontado = LocalDateTime.from(hoje);
 
+        long anos = tempoDescontado.until(show, ChronoUnit.YEARS);
+        tempoDescontado = tempoDescontado.plusYears(anos);
+        long meses = tempoDescontado.until(show, ChronoUnit.MONTHS);
+        tempoDescontado = tempoDescontado.plusMonths(meses);
+        long dias = tempoDescontado.until(show, ChronoUnit.DAYS);
+        tempoDescontado = tempoDescontado.plusDays(dias);
+        long horas = tempoDescontado.until(show, ChronoUnit.HOURS);
+        tempoDescontado = tempoDescontado.plusHours(horas);
+        long minutos = tempoDescontado.until(show, ChronoUnit.MINUTES);
+        tempoDescontado = tempoDescontado.plusMinutes(minutos);
+        long segundos = tempoDescontado.until(show, ChronoUnit.SECONDS);
 
+        System.out.println("Anos: " + anos);
+        System.out.println("Meses: " + meses);
+        System.out.println("Dias: " + dias);
+        System.out.println("Horas: " + horas);
+        System.out.println("Minutos: " + minutos);
+        System.out.println("Segundos: " + segundos);
 
     }
 }
